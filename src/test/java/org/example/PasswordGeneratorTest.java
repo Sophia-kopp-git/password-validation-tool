6b0707edf0d22fd5a013978bea7d22fc483815c8
sophiakopp <sophiakopp@gmx.de>
2025-08-27 13:09:59 +0200

    modified tests

--- src/test/java/org/example/PasswordGeneratorTest.java
+++ src/test/java/org/example/PasswordGeneratorTest.java
@@ -8,10 +8,14 @@ class PasswordGeneratorTest {
 
     @Test
     void generateValidPassword() {
+        String str = PasswordGenerator.generateValidPassword();
+        boolean expected = true;
+        boolean actual = PasswordValidator.isValidPassword(str);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void generateStringInUpperCase() {
+    void generateString() {
         int expectedLength = 2;
         String actual = PasswordGenerator.generateString();
         assertEquals(expectedLength, actual.length());
@@ -19,9 +23,23 @@ class PasswordGeneratorTest {
 
     @Test
     void generateNumber() {
+        String passwordNum = PasswordGenerator.generateNumber();
+        assertEquals(4, passwordNum.length());
+        assertTrue(PasswordValidator.containsDigit(passwordNum));
     }
 
     @Test
     void generateSpecialCharacter() {
+        String[] specialCharList = {"!", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "?", ".", ",", ";", ":"};
+        String str = PasswordGenerator.generateSpecialCharacter();
+        boolean expected = true;
+        boolean actual = false;
+        for (String specChar : specialCharList){
+            if (specChar.equals(str)){
+                actual = true;
+                break;
+            }
+        }
+        assertEquals(expected, actual);
     }
 }
