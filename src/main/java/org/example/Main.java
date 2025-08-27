4bc8786d0d806fd8d3368cb5f8c6d2764cccb5ac
sophiakopp <sophiakopp@gmx.de>
2025-08-27 12:04:52 +0200

    modified main method with error message

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -20,6 +20,22 @@ public class Main {
                 validPassword = true;
             } else {
                 System.out.println("Your password is invalid! Chose another one");
+                System.out.println("Make sure ... ");
+                if (!PasswordValidator.containsSpecialChar(password)) {
+                    System.out.println("- it has a special character");
+                }
+                if (!PasswordValidator.isCommonPassword(password)) {
+                    System.out.println("- it is not a common password");
+                }
+                if (!PasswordValidator.containsUpperAndLowerCase(password)) {
+                    System.out.println("- it contains at least one character in upper and lower case");
+                }
+                if (!PasswordValidator.containsDigit(password)) {
+                    System.out.println("- it contains at least one digit");
+                }
+            }
+            if (!PasswordValidator.hasMinLength(password)) {
+                System.out.println("- it is at least 8 characters long");
             }
         }
     }
